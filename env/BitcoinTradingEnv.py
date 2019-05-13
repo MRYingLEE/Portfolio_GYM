30bfd63dbca77835f63a6bc6a0ee8be581afb36a
Ying LI <Mr.Ying.LEE@gmail.com>
2019-05-12 21:06:21 -0400

    Add files via upload

--- env/BitcoinTradingEnv.py
+++ env/BitcoinTradingEnv.py
@@ -7,6 +7,22 @@ import datetime as dt
 from gym import spaces
 from sklearn import preprocessing
 
+import pyfolio
+
+from pyfolio import plotting
+from pyfolio import  perf_attrib
+import math
+import empyrical as ep
+
+from pyfolio.tears import (create_full_tear_sheet,
+                           create_simple_tear_sheet,
+                           create_returns_tear_sheet,
+                           create_position_tear_sheet,
+                           create_txn_tear_sheet,
+                           create_round_trip_tear_sheet,
+                           create_interesting_times_tear_sheet,
+                           create_bayesian_tear_sheet)
+
 MAX_TRADING_SESSION = 1000
 
 
@@ -15,7 +31,9 @@ class BitcoinTradingEnv(gym.Env):
     metadata = {'render.modes': ['human', 'system', 'none','metrics', 'save_metrics','save_epoche_data']} # I added a metrics mode
     scaler = preprocessing.MinMaxScaler()
 
-    def __init__(self, df, lookback_window_size=40, initial_balance=10000, commission=0.00075, serial=False):
+    def __init__(self, df, lookback_window_size=40, initial_balance=10000, commission=0.00075, serial=False,
+                 epoche_size=40, # walk ahead vs lookback
+                 ):
         super(BitcoinTradingEnv, self).__init__()
 
         self.df = df.dropna().reset_index()
@@ -23,6 +41,8 @@ class BitcoinTradingEnv(gym.Env):
         self.initial_balance = initial_balance
         self.commission = commission
         self.serial = serial
+        self.epoche_size = epoche_size #
+        self._epoche_steps = 0
 
         self.previous_net_worth = self.initial_balance
 
@@ -34,6 +54,9 @@ class BitcoinTradingEnv(gym.Env):
             low=0, high=1, shape=(10, lookback_window_size + 1), dtype=np.float16)
 
         self.net_worth_list=[]
+        self.returns=[]
+
+        self.metrics_history=[]
 
     def _next_observation(self):
         end = self.current_step + self.lookback_window_size + 1
@@ -88,7 +111,10 @@ class BitcoinTradingEnv(gym.Env):
             [0]
         ], self.lookback_window_size + 1, axis=1)
         self.trades = []
+        self.done = False
 
+        self.net_worth_list = []
+        self.returns = []
         return self._next_observation()
 
     def _get_current_price(self):
@@ -126,6 +152,8 @@ class BitcoinTradingEnv(gym.Env):
         self.previous_net_worth = self.net_worth
         self.net_worth = self.balance + self.btc_held * current_price
 
+        self.returns=np.append(self.returns, math.log(self.net_worth/self.previous_net_worth))
+
         #What, meaningless noise was added!
         self.account_history = np.append(self.account_history, [
             [self.net_worth],
@@ -151,9 +179,16 @@ class BitcoinTradingEnv(gym.Env):
 
         obs = self._next_observation()
         reward = self.net_worth - self.previous_net_worth # I substracted previous value
-        done = self.net_worth <= 0
+        self.done = (self.net_worth <= 0) | (self.current_step>=self.epoche_size)
 
-        return obs, reward, done, {}
+        if self.done:
+            # Todo 1. To save the epoche metrics to the environmnt -- System level
+            # Todo 2. To call user function to save the epoche data 
+            #self.save_epoche_data()
+            self.save_metrics()
+            self.returns=[]
+
+        return obs, reward, self.done, {}
 
     def render(self, mode='human', **kwargs):
         if mode == 'system':
@@ -165,29 +200,82 @@ class BitcoinTradingEnv(gym.Env):
             print('Net worth: ' + str(self.net_worth))
 
         elif mode == 'metrics':
-            #self.save_metrics()
-            print('Net worth: ' + str(self.net_worth))
-        elif mode == 'save_metrics':
-            self.save_metrics()
+            #Todo to call pyfolio to generate self.save_metrics()
+            if self.done: # Only when the epoche is done
+                print('Net worth: ' + str(self.net_worth))
+        elif mode == 'show_metrics':
+            self.show_epoche_metrics()
             
-        elif mode == 'to_csv':
-            self.save_epoche_data()
 
-    def clear_metrics(self):
+    def clear_metrics(self):# To use pyfolio
         self.net_worth_list=[]
+        self.returns=[]
 
     def save_metrics(self):
-        self.net_worth_list=np.append(self.net_worth_list,[self.net_worth],axis=0)
-        pd.DataFrame({"net_worth_list":self.net_worth_list}).to_csv("./metrics/net_worth_list.csv")
+        print('Net worth: ' + str(self.net_worth))
+        #self.net_worth_list=np.append(self.net_worth_list,[self.net_worth],axis=0)
+        #pd.DataFrame({"net_worth_list":self.net_worth_list}).to_csv("./metrics/net_worth_list.csv")
+        metrics=self.get_metrics()
+
+        self.metrics_history= np.append(self.metrics_history, metrics)
 
     def clear_epoche_data(self):
         self.trades = []
 
     def save_epoche_data(self):
-        pd.DataFrame(self.trades).to_csv("./metrics/trades_"+str(dt.datetime.now())+".csv")
+        #pd.DataFrame(self.trades).to_csv(".\\metrics\\trades_"+str(dt.datetime.now())+".csv")
+        print("save_epoche_data"+str(dt.datetime.now()))
+ 
+    def show_epoche_metrics(self):
+        dti=pd.date_range('2018-01-01', periods=self.returns.shape[0], freq='D')
+        ps=pd.Series(self.returns,dtype="float64",index=dti)
+        #plotting.show_perf_stats(ps,ps)
+        #create_returns_tear_sheet(ps,benchmark_rets=ps)
+        print(self.get_metrics())
  
     def close(self):
+        self.save_metrics() #Todo To save all metrics of epoches
         print("close")
 
+    def get_metrics(self):
+        SIMPLE_STAT_FUNCS = [
+    ep.annual_return,
+    ep.cum_returns_final,
+    ep.annual_volatility,
+    ep.sharpe_ratio,
+    ep.calmar_ratio,
+    ep.stability_of_timeseries,
+    ep.max_drawdown,
+    ep.omega_ratio,
+    ep.sortino_ratio #,
+    #stats.skew,
+    #stats.kurtosis,
+    #ep.tail_ratio,
+    #value_at_risk
+]
+        STAT_FUNC_NAMES = {
+    'annual_return': 'Annual return',
+    'cum_returns_final': 'Cumulative returns',
+    'annual_volatility': 'Annual volatility',
+    'sharpe_ratio': 'Sharpe ratio',
+    'calmar_ratio': 'Calmar ratio',
+    'stability_of_timeseries': 'Stability',
+    'max_drawdown': 'Max drawdown',
+    'omega_ratio': 'Omega ratio',
+    'sortino_ratio': 'Sortino ratio',
+    'skew': 'Skew',
+    'kurtosis': 'Kurtosis',
+    'tail_ratio': 'Tail ratio',
+    'common_sense_ratio': 'Common sense ratio',
+    'value_at_risk': 'Daily value at risk',
+    'alpha': 'Alpha',
+    'beta': 'Beta',
+}
+
+        stats = pd.Series()
+        for stat_func in SIMPLE_STAT_FUNCS:
+            stats[STAT_FUNC_NAMES[stat_func.__name__]] = stat_func(self.returns)
+
+        return stats
 
 
